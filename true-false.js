7c51b8083e1ffba4def22a07fef18bcbbf6ca48d
munmun sarker <munmuntravels2020@gmail.com>
2021-02-02 19:06:13 +0600

    trueand false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+//  FALSY
+// FALSE
+ // 0
+// ""
+// undefined
+// null
+// NAN
+// truTHY
+// "0"," " "FALSE"[] {}
+const name=0;
+
+if(name ||name==0){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");   
+}
