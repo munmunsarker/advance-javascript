bd06e2f416e4c43f6e2dcb6651f6bffae70e6004
munmun sarker <munmuntravels2020@gmail.com>
2021-02-02 19:40:12 +0600

    null vs undefine

---
+++ null-vs-undefine.js
@@ -0,0 +1,18 @@
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num01,num02){
+//     console.log(num01+num02);
+// }
+// var result=add(3,3)
+// console.log(result);
+
+// function add(num01,num02=4){
+//     console.log(num01,num02);
+// }
+// var result=add(3)
+// console.log(result);
+// const nayek={name:"omor",phn:01711,}
+// console.log(nayek.height);
+var number=[2,4,6];
+console.log(number[9]);
